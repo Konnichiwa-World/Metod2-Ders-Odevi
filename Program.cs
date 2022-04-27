087599a948a2015b381e72a9532e41466fdca1d6
Onur MUTLU <103672350+Konnichiwa-World@users.noreply.github.com>
2022-04-27 12:23:30 +0300

    PATİKA piramidi
    Tercihen, en az "6" sayısı girilmeli. Dizi ve for döngüsü kullanılmıştır.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Piramid{
+public static void Main(string[] args){
+int a,b,c,d,e;string[] Patika = {" ", "P","A","T","I","K","A"};
+System.Console.WriteLine("Luften, olusacak piramit için yukseklik belirtiniz:");
+Int32 z =Int32.Parse(Console.ReadLine());
+for(a=1;a<=z;a++){
+    if(a%2==1){
+    for(b=z;b>=a;b--){System.Console.Write(" ");}
+    for(c=1;c<=a;c++){System.Console.Write("*"+Patika[a]+"*");}
+    System.Console.WriteLine("");}
+    if(a%2==0){
+    for(d=z;d>=a;d--){System.Console.Write(" ");}
+    for(e=1;e<=a;e++){System.Console.Write("*"+Patika[a]+"*");}
+    System.Console.WriteLine("");}
+}
+}}
